508908fb08b3641c17898e78a5cbd63415be37fe
NgureWangai <ngurewangai@gmail.com>
2024-03-25 11:21:11 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*100);
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+let numberset = [3,7,12,20];
+let max = Math.max(...numberset);
 
-// Write your code here
+
+
